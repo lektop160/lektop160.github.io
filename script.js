e1da4d398b94f81a2cad81ce23e46d13966afa3e
lektop160 <118301747+lektop160@users.noreply.github.com>
2022-12-06 20:15:51 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,71 +5,25 @@ window.onload = function(){
 	clock.innerHTML = now.toLocaleTimeString();
 }, 1000);
 };
-
-
-
-// var zombig = document.querySelector('#zombig');
-// const lektop = document.querySelector('#lektop');
-
-/*zombig.onclick = function() {
-    const answer = +prompt('1-Yes, 0-No','');
-		console.log(answer);
-
-	if(zombig == 1){zombig.addEventListener('click', ()=>{
-		window.open('https://lektop159.github.io', '_blank');
-	})}else{}
-    }*/
-
-// zombig.addEventListener('click', ()=>{
-// 	window.open('https://lektop159.github.io', '_blank');
-// })
-
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/player_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+let lektop = document.querySelector('#lektop');
 lektop.addEventListener('click', ()=>{
 	window.open('https://lektop158.github.io', '_blank');
 })
-
 function AlertMsg() {
 var zombig = confirm ("You are getting redirected. Click OK to continue.")
 if (zombig)
 window.location="http://lektop159.github.io";
 }
-
-/*const result = confirm('Вам есть 18?')
-console.log(result);*/
-
-/*console.log(`https://lekop${answer}.github.io`)*/
-
-/*const answstud = +prompt('Сколько вам лет?','');
-
-if(answstud >= 18){
-	console.log('Добро пожаловать')
-} else if(answstud < 18){
-	console.log('Shit');
-} else{
-	console.log('Back');
-}*/
-
-/*let a = 1
-while(a < 100){
-	console.log(a);
-	a++
-}*/
-
 for(let i = 1; i<10; i++){
 	console.log(i);
 }
-
-/*function sayHello(){
-	console.log('Hello');
-}
-sayHello;
-
-function calc(a, b){
-	return(a+b);
-}
-
-calc(5, 2);
-
-let sum = calc(5, 2)
-console.log(sum);*/
-
+const massive = [1, 2, 3];
+massive.push(10);
+console.log(massive);
+massive.pop();
+console.log(massive);
+console.log(massive.join(','));
